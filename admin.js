41ffe555e9557b8f0e75030df2036d6c31cc90ae
bcashapp119-glitch <bcashapp119@gmail.com>
2026-08-05 10:41:31 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,65 +1,73 @@
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+
+import {
+getAuth,
+onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+
+
+const firebaseConfig = {
+apiKey: "AIzaSyDHwh66V6cT7nqEP9R7Iy827vbtBjQGeIA",
+authDomain: "nexora-mobile-af02f.firebaseapp.com",
+projectId: "nexora-mobile-af02f",
+storageBucket: "nexora-mobile-af02f.firebasestorage.app",
+messagingSenderId: "498265618137",
+appId: "1:498265618137:web:a1ffebae8eda0b88abf0c8",
+measurementId: "G-8494CNWZKL"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+
+onAuthStateChanged(auth, (user)=>{
+
+
+if(!user){
+
+window.location.href="login.html";
+
+return;
+
+}
+
+
+if(user.email !== "Sufiyanolawale36@gmail.com"){
+
+document.getElementById("app").innerHTML = `
+<h2 style="text-align:center;color:red;">
+Access Denied
+</h2>
+`;
+
+return;
+
+}
+
+
+// ADMIN DASHBOARD
+
 document.getElementById("app").innerHTML = `
 
-<div class="admin-container">
+<h1>Welcome to Nexora Admin Dashboard</h1>
 
-  <aside class="sidebar">
-    <h2>NEXORA</h2>
-    <p class="admin-title">ADMIN PANEL</p>
+<p>Admin: ${user.email}</p>
 
-    <nav>
-      <a>🏠 Dashboard</a>
-      <a>👥 Users</a>
-      <a>💳 Wallet</a>
-      <a>📱 Airtime</a>
-      <a>📶 Data</a>
-      <a>🌍 eSIM</a>
-      <a>📜 Transactions</a>
-      <a>⚙ Settings</a>
-    </nav>
-  </aside>
+<hr>
 
+<h3>Users</h3>
+<p>Manage Nexora users here</p>
 
-  <main class="dashboard">
+<h3>Wallet</h3>
+<p>Manage wallet transactions here</p>
 
-    <h1>Welcome to Nexora Admin Dashboard</h1>
-    <p class="subtitle">Administrator Control Center</p>
-
-
-    <div class="cards">
-
-      <div class="card">
-        <h3>Users</h3>
-        <p>0 Registered Users</p>
-      </div>
-
-      <div class="card">
-        <h3>Wallet</h3>
-        <p>₦0 Balance</p>
-      </div>
-
-      <div class="card">
-        <h3>Transactions</h3>
-        <p>0 Transactions</p>
-      </div>
-
-      <div class="card">
-        <h3>eSIM</h3>
-        <p>Management Ready</p>
-      </div>
-
-    </div>
-
-
-    <section class="panel">
-      <h2>System Status</h2>
-      <p>✅ Nexora Admin System Online</p>
-      <p>✅ Database Connected</p>
-      <p>✅ Services Ready</p>
-    </section>
-
-
-  </main>
-
-</div>
+<h3>Airtime / Data / eSIM</h3>
+<p>Service management ready</p>
 
 `;
+
+});
