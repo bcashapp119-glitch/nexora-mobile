e09443d7360136b33879b6938ee7fb0488827d29
bcashapp119-glitch <bcashapp119@gmail.com>
2026-08-05 10:56:08 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,21 +1,25 @@
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+
+import { 
+  getAuth,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
 
 import {
-getAuth,
-onAuthStateChanged
-}
-from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
 
 const firebaseConfig = {
-apiKey: "AIzaSyDHwh66V6cT7nqEP9R7Iy827vbtBjQGeIA",
-authDomain: "nexora-mobile-af02f.firebaseapp.com",
-projectId: "nexora-mobile-af02f",
-storageBucket: "nexora-mobile-af02f.firebasestorage.app",
-messagingSenderId: "498265618137",
-appId: "1:498265618137:web:a1ffebae8eda0b88abf0c8",
-measurementId: "G-8494CNWZKL"
+  apiKey: "AIzaSyDHwh66V6cT7nqEP9R7Iy827vbtBjQGeIA",
+  authDomain: "nexora-mobile-af02f.firebaseapp.com",
+  projectId: "nexora-mobile-af02f",
+  storageBucket: "nexora-mobile-af02f.firebasestorage.app",
+  messagingSenderId: "498265618137",
+  appId: "1:498265618137:web:a1ffebae8eda0b88abf0c8",
+  measurementId: "G-8494CNWZKL"
 };
 
 
@@ -23,14 +27,16 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
+const db = getFirestore(app);
 
-onAuthStateChanged(auth, (user)=>{
+
+
+onAuthStateChanged(auth, async (user)=>{
 
 
 if(!user){
 
 window.location.href="login.html";
-
 return;
 
 }
@@ -39,7 +45,7 @@ return;
 if(user.email !== "Sufiyanolawale36@gmail.com"){
 
 document.getElementById("app").innerHTML = `
-<h2 style="text-align:center;color:red;">
+<h2 style="color:red;text-align:center;">
 Access Denied
 </h2>
 `;
@@ -49,24 +55,72 @@ return;
 }
 
 
-// ADMIN DASHBOARD
+
+let usersHTML = "";
+
+
+const usersSnapshot = await getDocs(collection(db,"users"));
+
+
+usersSnapshot.forEach((doc)=>{
+
+const data = doc.data();
+
+
+usersHTML += `
+
+<div style="
+background:white;
+padding:15px;
+margin:10px;
+border-radius:10px;
+">
+
+<h3>${data.name || "User"}</h3>
+
+<p>Email: ${data.email || "No email"}</p>
+
+<p>Status: ${data.status || "Unknown"}</p>
+
+<p>Wallet: ₦${data.wallet || 0}</p>
+
+</div>
+
+`;
+
+});
+
+
 
 document.getElementById("app").innerHTML = `
 
-<h1>Welcome to Nexora Admin Dashboard</h1>
+<h1>Nexora Admin Dashboard</h1>
 
-<p>Admin: ${user.email}</p>
+<p>
+Admin: ${user.email}
+</p>
+
 
 <hr>
 
-<h3>Users</h3>
-<p>Manage Nexora users here</p>
 
-<h3>Wallet</h3>
-<p>Manage wallet transactions here</p>
+<h2>Users</h2>
 
-<h3>Airtime / Data / eSIM</h3>
-<p>Service management ready</p>
+${usersHTML}
+
+
+
+<h2>Wallet</h2>
+<p>Wallet management coming next</p>
+
+
+<h2>Transactions</h2>
+<p>Transaction management coming next</p>
+
+
+<h2>Airtime / Data / eSIM</h2>
+<p>Service management coming next</p>
+
 
 `;
 
