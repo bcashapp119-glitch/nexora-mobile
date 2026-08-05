2e42da38faa55d01eea3dd81624ddf0c7cfd7539
bcashapp119-glitch <bcashapp119@gmail.com>
2026-08-05 09:17:06 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,44 @@
+document.getElementById("app").innerHTML = `
 
+<div style="display:flex;width:100%;">
+
+<div style="
+width:240px;
+background:#1F2937;
+padding:20px;
+min-height:100vh;
+">
+
+<h2 style="margin-bottom:30px;">
+NEXORA ADMIN
+</h2>
+
+<p>🏠 Dashboard</p>
+<p>👥 Users</p>
+<p>💰 Wallets</p>
+<p>📱 Airtime</p>
+<p>📶 Data</p>
+<p>🌍 eSIM Store</p>
+<p>📜 Transactions</p>
+<p>⚙ Settings</p>
+
+</div>
+
+<div style="
+flex:1;
+padding:30px;
+background:#F3F4F6;
+color:#111827;
+">
+
+<h1>Welcome, Administrator</h1>
+
+<p>
+Nexora Admin Dashboard v2.0 has started successfully.
+</p>
+
+</div>
+
+</div>
+
+`;
